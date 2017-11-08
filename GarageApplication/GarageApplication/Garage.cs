7eb5d460a4583c1b49ebef80f5307c024c93b09c
bhanu18v <bhanu18v@gmail.com>
2017-11-08 16:59:53 +0100

    TuesdayGarageEOD

---
+++ GarageApplication/GarageApplication/Garage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageApplication
+{
+   
+        class Garage<T> : IEnumerable<T> where T : Vehicle
+        {
+            public int ParkingNumber = 0;
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public List<T> Vehicles { get; set; }
+            private int capacity;
+            private string name;
+            public int Capacity { get; set; }
+
+            private static int parkingNumber = 0;
+
+
+            public Garage(int maxCapacity)
+            {
+                this.Id = parkingNumber++;
+                this.Name = name;
+                this.Vehicles = new List<T>();
+                this.Capacity = maxCapacity;
+            }
+
+
+            public override string ToString()
+            {
+                //return string.Format("{1, 30} {2, 3}", Id, Name, Vehicles.Count);
+                return string.Format(Name, Vehicles.Count, Capacity);
+            }
+
+
+            public IEnumerator<T> GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+    }
+}
---
+++ GarageApplication/GarageApplication/Vehicle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageApplication
+{
+  public  class Vehicle
+    {
+        private string registrationNumber;
+        private string color;
+        private int no_Of_Wheels;
+        private string modelType;
+        public string RegistrationNumber { get; set; }
+        public string Color { get; set; }
+        public int No_Of_Wheels { get; set; }
+        public string ModelType { get; set; }
+        public Vehicle()
+        {
+
+        }
+        public Vehicle(string registrationNumber, string color, int no_Of_Wheels, string modelType)
+        {
+            RegistrationNumber = registrationNumber;
+            Color = color;
+            No_Of_Wheels = no_Of_Wheels;
+            ModelType = modelType;
+
+        }
+
+    }
+}
